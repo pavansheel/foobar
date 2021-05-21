cc79a2486d8f082f0d2dfe4e3052e459c555d7d4
Pavan Bhatnagar <pavansb@grainite.com>
2021-05-21 07:36:32 +0000

    Send multiple append requests

--- example/topicappend.go
+++ example/topicappend.go
@@ -16,7 +16,7 @@ const (
 )
 
 func main() {
-	// set up a connection
+	// Set up a connection.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
@@ -24,22 +24,25 @@ func main() {
 	defer conn.Close()
 	grainite := proto.NewGrainiteClient(conn)
 
-	// append 100 events to topic 'mytopic', using 20 keys
+	// Send 5 append requests. Each appends 100 events to topic 'mytopic',
+	// using 20 keys.
 	req := &proto.AppendRequest{}
 	req.App = "myapp"
 	req.TopicName = "mytopic"
-	for i := 0; i < 100; i++ {
-		var key string = fmt.Sprintf("key_%d", i%20)
-		var payload string = fmt.Sprintf("payload_%d", i)
-		event := &proto.Event{Key: []byte(key), Payload: []byte(payload)}
-		req.Events = append(req.Events, event)
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	resp, err := grainite.TopicAppend(ctx, req)
-	if err != nil {
-		log.Fatalf("could not Append: %v", err)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 100; j++ {
+			var key string = fmt.Sprintf("key_%d", j%20)
+			var payload string = fmt.Sprintf("payload_%d", (i*100)+j)
+			event := &proto.Event{Key: []byte(key), Payload: []byte(payload)}
+			req.Events = append(req.Events, event)
+		}
+		resp, err := grainite.TopicAppend(ctx, req)
+		if err != nil {
+			log.Fatalf("Could not Append: %v", err)
+		}
+		// print out the response
+		log.Printf("Append result: %v", resp)
 	}
-	// print out the response
-	log.Printf("append successful: %v", resp)
 }
